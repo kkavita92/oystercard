8ddf479e3b22911f426deb4035599834be83b9e9
Kavita <kkavita92@gmail.com>
2017-05-15 23:20:53 +0100

    Adds test which checks if error is raised when top up exceeds limit

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,10 @@ describe Oystercard do
     expect(subject.balance).to eq top_up_amount
   end
 
+  describe '#top_up' do
+    it "should raise an error when top_up_amount would cause card's balance to exceed maximum balance" do
+      expect { subject.top_up(Oystercard::MAX_BALANCE + 1) }.to raise_error(BalanceError)
+    end
+  end
+
 end
