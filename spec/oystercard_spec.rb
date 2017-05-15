fc9c893f2aa2c4ce7eb746ef0d6a857541fc1ae6
Kavita <kkavita92@gmail.com>
2017-05-15 23:13:54 +0100

    Complete User Story 1 : Allowing new card to have default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,7 @@
+require 'oystercard'
+
 describe Oystercard do
-end 
+  it 'should be able to test that a freshly initialized card has a balance of 0 by default' do
+    expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
+  end
+end
