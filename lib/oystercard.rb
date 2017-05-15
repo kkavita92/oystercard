f15d3cba1c393c261b85de900794d27344f8b7f1
Kavita <kkavita92@gmail.com>
2017-05-15 23:17:17 +0100

    Implement User Story 2/3 - Enable top up functionality with #top_up method - Enforce maximum balance so that error is raised if customer top up exceeds limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,18 @@
+class BalanceError < StandardError; end
+
 class Oystercard
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
-  
+  MAX_BALANCE = 90
+
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
+
+  def top_up(top_up_amount)
+  raise(BalanceError, 'requested top-up amount exceeds limit!') if (@balance + top_up_amount) > MAX_BALANCE
+  @balance += top_up_amount
+end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'should be able to test that a freshly initialized card has a balance of 0 by default' do
     expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
   end
+
+  it "should enable an oystercard's balance to be topped up" do
+    top_up_amount = 10
+    subject.top_up(top_up_amount)
+    expect(subject.balance).to eq top_up_amount
+  end
+
 end
